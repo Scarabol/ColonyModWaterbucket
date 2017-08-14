2beee27d782e65e1860eab68c92ea33d74440a21
Scarabol <scarabol@gmail.com>
2017-08-15 01:24:32 +0200

    add localization, icons and bucket textures

--- src/Waterbucket.cs
+++ src/Waterbucket.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using Pipliz;
 using Pipliz.Chatting;
 using Pipliz.JSON;
@@ -10,6 +12,22 @@ namespace ScarabolMods
   [ModLoader.ModManager]
   public class WaterbucketModEntries
   {
+    public static string MOD_PREFIX = "mods.scarabol.waterbucket.";
+    private static string AssetsDirectory;
+    private static string RelativeTexturesPath;
+    private static string RelativeIconsPath;
+
+    [ModLoader.ModCallback(ModLoader.EModCallbackType.OnAssemblyLoaded, "scarabol.waterbucket.assemblyload")]
+    public static void OnAssemblyLoaded(string path)
+    {
+      string ModDirectory = Path.GetDirectoryName(path);
+      AssetsDirectory = Path.Combine(ModDirectory, "assets");
+      ModLocalizationHelper.localize(Path.Combine(AssetsDirectory, "localization"), MOD_PREFIX, false);
+      // TODO this is realy hacky (maybe better in future ModAPI)
+      RelativeTexturesPath = new Uri(MultiPath.Combine(Path.GetFullPath("gamedata"), "textures", "materials", "blocks", "albedo", "dummyfile")).MakeRelativeUri(new Uri(MultiPath.Combine(AssetsDirectory, "textures", "albedo"))).OriginalString;
+      RelativeIconsPath = new Uri(MultiPath.Combine(Path.GetFullPath("gamedata"), "textures", "icons", "dummyfile")).MakeRelativeUri(new Uri(MultiPath.Combine(AssetsDirectory, "icons"))).OriginalString;
+    }
+
     [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterStartup, "scarabol.waterbucket.registercallbacks")]
     public static void AfterStartup()
     {
@@ -19,17 +37,41 @@ namespace ScarabolMods
     [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterAddingBaseTypes, "scarabol.waterbucket.addrawtypes")]
     public static void AfterAddingBaseTypes()
     {
-      ItemTypes.AddRawType("waterbucket",
-        new JSONNode(NodeType.Object)
-          .SetAs("maxStackSize", 5)
-          .SetAs("needsBase", "true")
-          .SetAs("npcLimit", 0)
+      ItemTypesServer.AddTextureMapping(MOD_PREFIX + "bucketSide", new JSONNode()
+        .SetAs("albedo", "furnaceSide")
+        .SetAs("normal", "neutral")
+        .SetAs("emissive", "neutral")
+        .SetAs("height", "neutral")
       );
-      ItemTypes.AddRawType("waterbucketfilled",
+      ItemTypesServer.AddTextureMapping(MOD_PREFIX + "bucketemptytop", new JSONNode()
+        .SetAs("albedo", Path.Combine(RelativeTexturesPath, "bucketEmptyTop"))
+        .SetAs("normal", "furnaceUnlitTop")
+        .SetAs("emissive", "neutral")
+        .SetAs("height", "neutral")
+      );
+      ItemTypes.AddRawType(MOD_PREFIX + "bucket",
         new JSONNode(NodeType.Object)
+          .SetAs("icon", Path.Combine(RelativeIconsPath, "bucket.png"))
           .SetAs("maxStackSize", 5)
           .SetAs("needsBase", "true")
           .SetAs("npcLimit", 0)
+          .SetAs("sideall", MOD_PREFIX + "bucketSide")
+          .SetAs("sidey+", MOD_PREFIX + "bucketemptytop")
+      );
+      ItemTypesServer.AddTextureMapping(MOD_PREFIX + "waterbucketfilledtop", new JSONNode()
+        .SetAs("albedo", Path.Combine(RelativeTexturesPath, "waterbucketFilledTop"))
+        .SetAs("normal", "furnaceUnlitTop")
+        .SetAs("emissive", "neutral")
+        .SetAs("height", "neutral")
+      );
+      ItemTypes.AddRawType(MOD_PREFIX + "waterbucket",
+        new JSONNode(NodeType.Object)
+          .SetAs("icon", Path.Combine(RelativeIconsPath, "waterbucket.png"))
+          .SetAs("maxStackSize", 5)
+          .SetAs("needsBase", "true")
+          .SetAs("npcLimit", 0)
+          .SetAs("sideall", MOD_PREFIX + "bucketSide")
+          .SetAs("sidey+", MOD_PREFIX + "waterbucketfilledtop")
       );
     }
 
@@ -37,7 +79,7 @@ namespace ScarabolMods
     public static void AfterItemTypesServer()
     {
       ItemTypesServer.RegisterOnRemove("water", WaterbucketCode.OnWaterRemoved);
-      ItemTypesServer.RegisterOnAdd("waterbucketfilled", WaterbucketCode.OnAddFilled);
+      ItemTypesServer.RegisterOnAdd(MOD_PREFIX + "waterbucket", WaterbucketCode.OnAddFilled);
     }
 
     [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesDefined, "scarabol.waterbucket.loadrecipes")]
@@ -45,7 +87,7 @@ namespace ScarabolMods
     public static void AfterItemTypesDefined()
     {
       RecipePlayer.AllRecipes.Add(new Recipe(new JSONNode()
-        .SetAs("results", new JSONNode(NodeType.Array).AddToArray(new JSONNode().SetAs("type", "waterbucket")))
+        .SetAs("results", new JSONNode(NodeType.Array).AddToArray(new JSONNode().SetAs("type", MOD_PREFIX + "bucket")))
         .SetAs("requires", new JSONNode(NodeType.Array).AddToArray(new JSONNode().SetAs("type", "ironingot").SetAs("amount", 3)))
       ));
     }
@@ -58,10 +100,11 @@ namespace ScarabolMods
       ThreadManager.InvokeOnMainThread(delegate ()
       {
         ushort newType;
-        if (World.TryGetTypeAt(position, out newType) && newType == ItemTypes.IndexLookup.GetIndex("waterbucket")) {
-          ServerManager.TryChangeBlock(position, ItemTypes.IndexLookup.GetIndex("air"));
-          Stockpile.GetStockPile(causedBy).Add(ItemTypes.IndexLookup.GetIndex("waterbucketfilled"), 1);
-          Chat.Send(causedBy, string.Format("filled water bucket added to your stockpile"));
+        if (World.TryGetTypeAt(position, out newType) && newType == ItemTypes.IndexLookup.GetIndex(WaterbucketModEntries.MOD_PREFIX + "bucket")) {
+          if (ServerManager.TryChangeBlock(position, ItemTypes.IndexLookup.GetIndex("air"))) {
+            Stockpile.GetStockPile(causedBy).Add(ItemTypes.IndexLookup.GetIndex(WaterbucketModEntries.MOD_PREFIX + "waterbucket"), 1);
+            Chat.Send(causedBy, string.Format("water filled bucket added to your stockpile"));
+          }
         }
       }, 0.5);
     }
@@ -71,11 +114,12 @@ namespace ScarabolMods
       ThreadManager.InvokeOnMainThread(delegate ()
       {
         ushort actualType;
-        if (World.TryGetTypeAt(position, out actualType) && actualType == ItemTypes.IndexLookup.GetIndex("waterbucketfilled")) {
+        if (World.TryGetTypeAt(position, out actualType) && actualType == ItemTypes.IndexLookup.GetIndex(WaterbucketModEntries.MOD_PREFIX + "waterbucket")) {
           Chat.SendToAll(string.Format("{0} spilled some water at {1}", causedBy.Name, position));
-          ServerManager.TryChangeBlock(position, ItemTypes.IndexLookup.GetIndex("water"));
-          Stockpile.GetStockPile(causedBy).Add(ItemTypes.IndexLookup.GetIndex("waterbucket"), 1);
-          Chat.Send(causedBy, string.Format("empty water bucket added to your stockpile"));
+          if (ServerManager.TryChangeBlock(position, ItemTypes.IndexLookup.GetIndex("water"))) {
+            Stockpile.GetStockPile(causedBy).Add(ItemTypes.IndexLookup.GetIndex(WaterbucketModEntries.MOD_PREFIX + "bucket"), 1);
+            Chat.Send(causedBy, string.Format("bucket added to your stockpile"));
+          }
         }
       }, 0.5);
     }
